0431df9f6ed27411377d831dc70dfc8ecd2148f4
Andrea Child <andrea.child@improving.com>
2025-06-05 21:54:47 -0700

    Added integration test for CDK pipeline with SDK resolver.

--- test/TestCases/Case06/Case06.02.test.js
+++ test/TestCases/Case06/Case06.02.test.js
@@ -1,19 +1,40 @@
-import { checkFileContains, readJSONFile, testCdkArtifacts } from '../../testLib';
+import { checkFileContains, readJSONFile, unzipAndGetContents } from '../../testLib';
 import fs from "fs";
+import path from "path";
 
-const casetest = readJSONFile('./test/TestCases/Case06/case.json');
+const casetest = readJSONFile('./test/TestCases/Case06/case01.json');
 let neptuneType = 'neptune-db';
 if (casetest.host.includes('neptune-graph')) {
     neptuneType = 'neptune-graph';
 }
-describe('Validate file content', () => {
-    afterAll(async () => {
-        fs.rmSync('./test/TestCases/Case06/output', {recursive: true});
-    });
-    
-    testCdkArtifacts('./test/TestCases/Case06/output', 'AirportCDKTestJest');
+const outputFolderPath = './test/TestCases/Case06/case01-output';
 
-    checkFileContains('./test/TestCases/Case06/output/AirportCDKTestJest-cdk.js', [
+describe('Validate cdk pipeline with http resolver output content', () => {
+    afterAll(async () => {
+        fs.rmSync(outputFolderPath, {recursive: true});
+    });
+
+    test('Zip file contains expected files', async () => {
+        const expectedFiles = [
+            'index.mjs',
+            'node_modules',
+            'output.resolver.graphql.js',
+            'output.resolver.schema.json',
+            'package-lock.json',
+            'package.json',
+            'queryHttpNeptune.mjs'
+        ];
+        const unzippedFolder = path.join(outputFolderPath, 'cdk-unzipped');
+        const actualFiles = unzipAndGetContents(unzippedFolder, path.join(outputFolderPath, 'AirportCDKTestJest.zip'));
+        expect(actualFiles.toSorted()).toEqual(expectedFiles.toSorted());
+        
+        // resolver should be using axios http client
+        const fileContent = fs.readFileSync(path.join(unzippedFolder, 'index.mjs'), 'utf8');
+        expect(fileContent).toContain('axios');
+    });
+
+
+    checkFileContains(path.join(outputFolderPath, 'AirportCDKTestJest-cdk.js'), [
         'const NAME = \'AirportCDKTestJest\';',
         'const NEPTUNE_HOST = \'' + casetest.host + '\';',
         'const NEPTUNE_PORT = \'' + casetest.port + '\';',
